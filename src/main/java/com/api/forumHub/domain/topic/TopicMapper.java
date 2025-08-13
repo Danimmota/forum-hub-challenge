14c7862e5258d41f2fc3ea0a27af80576e2d35bd
Daniela Medeiro Mota <danielamedeiromota@hotmail.com>
2025-08-13 03:32:49 -0300

    test(api): adiciona test de criação de tópicos e respostas

--- src/main/java/com/api/forumHub/domain/topic/TopicMapper.java
+++ src/main/java/com/api/forumHub/domain/topic/TopicMapper.java
@@ -1,8 +1,13 @@
 package com.api.forumHub.domain.topic;
 
 import com.api.forumHub.domain.answer.AnswerMapper;
+import com.api.forumHub.domain.course.Course;
+import com.api.forumHub.domain.course.CourseDTO;
+import com.api.forumHub.domain.user.User;
 import com.api.forumHub.domain.user.UserMapper;
 import org.springframework.stereotype.Component;
+
+import java.time.LocalDateTime;
 
 @Component
 public class TopicMapper {
@@ -17,5 +22,15 @@ public class TopicMapper {
                 topic.getCourse().getId(),
                 UserMapper.toDto(topic.getAuthor()),
                 topic.getAnswers().stream().map(AnswerMapper::toDto).toList());
+    }
+
+    public static Object toEntity(TopicRequest request) {
+        Topic topic = new Topic();
+        topic.setTitle(request.title());
+        topic.setMessage(request.message());
+        topic.setCreationDate(LocalDateTime.now());
+        topic.setStatus(TopicStatus.PENDENTE);
+
+        return topic;
     }
 }
